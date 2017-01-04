f4282e43fc7cb4d5452f137dafe3453aa4c723c6
Peter Witham <peter@peterwitham.com>
2017-01-03 23:02:10 -0600

    Xcode Project Setup
    Initial iPad Master/Detail view project creation. Also configured .gitignore for Xcode and Swift using gitignore.io

---
+++ Code Notes/Code Notes/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Code Notes
+//
+//  Created by Peter Witham on 1/3/17.
+//  Copyright © 2017 Peter Witham. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
